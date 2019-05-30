7510ae4da472f000fffc16b3cde676a7c8ca76c3
Diana Badas <diana.badas@gmail.com>
2019-05-30 08:50:39 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -25,4 +25,9 @@ class CategoriesController < ApplicationController
     category.update(params.require(:category))
     redirect_to category_path(category)
   end
+
+  private
+  def category_params
+
+  end
 end
